6d2e1417192a3e525f1957492fc37c3189a4d7ea
Sumitdurgam <sumitdurgam12@gmail.com>
2024-09-12 23:24:00 +0530

    counter App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Xcounter from "./Xcounter/Xcounter";
+
+function App() {
+  return (
+    <div className="App">
+      <Xcounter />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Xcounter/Xcounter.js
@@ -0,0 +1,34 @@
+import React, {  useState } from 'react';
+// import React from "react";
+// import './xcounter.css';
+
+function Xcounter() {
+    const [count, setCount] = useState(0);
+
+
+    const increament = () => {
+        setCount(count + 1);
+    };
+
+    const decreamnet =() => {
+        setCount(count - 1);
+    };
+
+
+    return(
+        <div style={{ textAlign: 'left', paddingLeft: '0.70%' }}>
+            <h1> Counter App</h1>
+            <p>Count: {count}</p>
+            <div>
+                <button onClick={increament}>Increament</button>
+                <button onClick={decreamnet}>Decreamnet</button>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+
+export default Xcounter;
