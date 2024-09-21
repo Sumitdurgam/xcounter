25a71420bcef78b9ee9b7fa04592d09f50bb81b3
Sumitdurgam <sumitdurgam12@gmail.com>
2024-09-21 16:38:09 +0530

    count

--- src/Xcounter/Xcounter.js
+++ src/Xcounter/Xcounter.js
@@ -1,24 +1,22 @@
 import React, {  useState } from 'react';
-// import React from "react";
-// import './xcounter.css';
 
 function Xcounter() {
-    const [count, setCount] = useState(0);
+    const [valueOfCount, setValueOfCount] = useState(0);
 
 
     const handleIncrement = () => {
-        setCount(prevCount => prevCount + 1); 
+        setValueOfCount(prevCount => prevCount + 1); 
     };
 
     const handleDecrement =() => {
-        setCount(prevCount => prevCount - 1); 
+        setValueOfCount(prevCount => prevCount - 1); 
     };
 
 
     return(
         <div style={{ textAlign: 'left', paddingLeft: '0.70%' }}>
             <h1> Counter App</h1>
-            <p>Count: {count}</p>
+            <p>Count: {valueOfCount}</p>
             <div>
                 <button onClick={handleIncrement}>Increament</button>
                 <button onClick={handleDecrement}>Decreamnet</button>
